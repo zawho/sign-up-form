4198e89f97cb65a9b14157be9684ddc82fc1982c
zawho <zthooper2@gmail.com>
2022-12-14 10:21:02 +0800

    Fix JS DOM element selectors

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const firstPass = document.querySelector('.first-password');
-const confirmPass = document.querySelector('.confirm-password');
+const firstPass = document.getElementById('first-password');
+const confirmPass = document.getElementById('confirm-password');
+const form = document.getElementById('sign-up');
 const submitButton = document.querySelector('.submit-button');
