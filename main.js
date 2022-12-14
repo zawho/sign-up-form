57842803e798a965874aef31fd3b6bbee690e687
zawho <zthooper2@gmail.com>
2022-12-14 09:27:54 +0800

    Add JS file

---
+++ main.js
@@ -0,0 +1,3 @@
+const firstPass = document.querySelector('.first-password');
+const confirmPass = document.querySelector('.confirm-password');
+const submitButton = document.querySelector('.submit-button');
